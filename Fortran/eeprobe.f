2de5e0a630139b2f28443f2c55120bb7564fd53c
Loïc Cudennec <loic.cudennec@gmail.com>
2020-04-03 20:15:31 +0200

    Fortran: micro-sleep

--- Fortran/eeprobe.f
+++ Fortran/eeprobe.f
@@ -18,24 +18,131 @@ c$$$    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 c$$$ ----------------------------------------------------------------------------------
 
+      MODULE EEPROBE_Parameters
+      implicit none
+      
+      integer*8 :: EEPROBE_LAST_YIELD_TIME = 0
+
+      integer*8 :: EEPROBE_MAX_YIELD_TIME = 0
+
+      integer*8 :: EEPROBE_MIN_YIELD_TIME = 0
+
+      integer*8 :: EEPROBE_INC_YIELD_TIME = 0
+
+      integer*8 :: EEPROBE_TOTAL_SLEEP_TIME = 0
+      
+      END MODULE EEPROBE_Parameters
+
+      
+c$$$ ----------------------------------------------------------------------------------
+
+      MODULE EEPROBE_Sleep
+      
+      use iso_c_binding
+      implicit none 
+      
+      interface
+
+         SUBROUTINE usleep(USECONDS) bind(C) 
+         use iso_c_binding
+         implicit none
+         integer(c_int64_t), value :: USECONDS 
+         END SUBROUTINE usleep
+      
+      END interface
+      
+      END MODULE EEPROBE_Sleep
+
+c$$$ ----------------------------------------------------------------------------------
+      
       MODULE EEProbe
       PUBLIC EEPROBE_getTime
+      PUBLIC EEPROBE_Probe
       CONTAINS
+
+c$$$ ----------------------------------------------------------------------------------
+
+
+
       
       integer FUNCTION EEPROBE_getTime()
 
-        real :: t
-        integer :: it
+      real :: t
+      integer :: it
       
-        call CPU_TIME(t)
+      call CPU_TIME(t)
 
-        t = t * 1000000
-        it = INT(t)
+      t = t * 1000000
+      it = INT(t)
         
-        EEPROBE_getTime = it
-        return
+      EEPROBE_getTime = it
+      return
+
       END FUNCTION EEPROBE_getTime
       
-      END MODULE
+c$$$ ----------------------------------------------------------------------------------
+
+      SUBROUTINE EEPROBE_USleep(USECONDS)
+
+      use EEPROBE_Sleep
+      use iso_c_binding
+      
+      implicit none
+
+      integer(c_int64_t), value :: USECONDS
+      
+      CALL usleep(USECONDS)
+      
+      END SUBROUTINE EEPROBE_USleep
       
 c$$$ ----------------------------------------------------------------------------------
+
+      SUBROUTINE EEPROBE_Probe(SOURCE, TAG, COMM, STATUS, IERROR,
+     &     ENABLE)
+
+      use EEPROBE_Parameters
+      
+      integer SOURCE, TAG, COMM, STATUS, IERROR
+      logical ENABLE
+
+      logical :: FLAG = .FALSE.
+      
+      integer*8 :: current_yield_duration = 0
+      integer :: start = 0
+      
+      if (ENABLE) then
+         
+ 10      CALL MPI_IPROBE(SOURCE, TAG, COMM, FLAG, STATUS, IERROR)
+         if ((.NOT. FLAG) .AND. (IERROR == MPI_SUCCESS)) then
+
+            start = EEPROBE_getTime()
+            
+            CALL EEPROBE_USleep(current_yield_duration)
+
+            EEPROBE_TOTAL_SLEEP_TIME = EEPROBE_TOTAL_SLEEP_TIME
+     &           + EEPROBE_getTime() - start
+
+            current_yield_duration = current_yield_duration
+     &           + EEPROBE_INC_YIELD_TIME
+            if (current_yield_duration > EEPROBE_MAX_YIELD_TIME) then
+               current_yield_duration = EEPROBE_MAX_YIELD_TIME
+            end if
+            
+            
+            goto 10
+         end if
+
+         EEPROBE_LAST_YIELD_TIME = current_yield_duration
+         
+      else
+
+         call MPI_PROBE(SOURCE, TAG, COMM, STATUS, IERROR)
+
+      end if
+      
+
+      END SUBROUTINE EEPROBE_Probe
+
+c$$$ ----------------------------------------------------------------------------------
+
+      END MODULE
--- Fortran/eetest.f
+++ Fortran/eetest.f
@@ -18,11 +18,13 @@ c$$$    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 c$$$ ----------------------------------------------------------------------------------
 
-      SUBROUTINE EEPROBE_SendRecv()
+      SUBROUTINE EEPROBE_SendRecv(ENABLE)
 
       use mpi
       use EEProbe
 
+      logical ENABLE
+      
       integer :: EEPROBE_COUNT = 1          
       integer :: EEPROBE_TAG = 0
       integer :: EEPROBE_RANK_SEND = 0
@@ -30,7 +32,8 @@ c$$$ ---------------------------------------------------------------------------
       integer :: EEPROBE_NB_ITER = 24
       integer :: EEPROBE_INTER_MSG_SLEEP_S = 1
       
-      integer rank, er
+      integer rank, status
+      integer :: er = MPI_SUCCESS
       integer start_time, t
       integer :: i = 0
       integer :: buffer = 42
@@ -53,19 +56,23 @@ c$$$ ---------------------------------------------------------------------------
             i = i + 1
             call sleep(EEPROBE_INTER_MSG_SLEEP_S)
             goto 10
-         endif
+         end if
          
       else
          
  20      if (i <EEPROBE_NB_ITER) then
+            call EEPROBE_Probe(EEPROBE_RANK_SEND, EEPROBE_TAG,
+     &           MPI_COMM_WORLD, status, er, ENABLE)
+            
             call MPI_RECV(buffer, EEPROBE_COUNT, MPI_INTEGER,
      &           EEPROBE_RANK_SEND, EEPROBE_TAG,
      &           MPI_COMM_WORLD, MPI_STATUS_IGNORE, er)
+            
             t = EEPROBE_getTime() - start_time
             print *, t, "rank", rank, "recv", i
             i = i + 1
             goto 20
-         endif
+         end if
            
       end if
       
@@ -75,7 +82,11 @@ c$$$ ---------------------------------------------------------------------------
 c$$$ ----------------------------------------------------------------------------------
 
       PROGRAM eetest
-        
+
+      use mpi
+      
+      logical :: enable = .TRUE.
+      
       CHARACTER(len=32) arg
       CHARACTER(len=32) bin_name
       integer rank, nr, er
@@ -85,8 +96,9 @@ c$$$ ---------------------------------------------------------------------------
       call getarg(0, bin_name)
       call getarg(1, arg)
 
-c$$$        if (arg == "disable") then
-c$$$        endif
+      if (arg == "disable") then
+         enable = .FALSE.
+      end if
         
       call MPI_INIT(er)
       call MPI_COMM_SIZE(MPI_COMM_WORLD, nr, er)
@@ -98,7 +110,7 @@ c$$$        endif
 
       if (nr == 2) then
 
-         call EEPROBE_SendRecv()
+         call EEPROBE_SendRecv(enable)
          
       else
 
